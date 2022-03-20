c3705f69b6052cbbbc34e073d1cd884007a7316e
harshverma036 <harshverma0362@gmail.com>
2022-03-20 15:48:02 +0530

    browser window setup done

--- index.js
+++ index.js
@@ -2,3 +2,16 @@ const { app, BrowserWindow } = require('electron');
 const url = require('url')
 const path = require('path')
 
+let mainWindow;
+
+// listen for app to be ready
+app.on('ready', () => {
+    // create new window
+    mainWindow = new BrowserWindow();
+    // load html
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+});
